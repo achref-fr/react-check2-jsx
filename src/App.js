a45773860b7b1ce93bdb8cbdfe225da6edf92a8f
achref-fr <achraf.riahi@esprit.tn>
2022-06-13 10:21:46 +0200

    insta check 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import './style.css';
+import imageInSrc from "./img/imageInSrc.jpg"
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="container">
+        <div style={{border:"solid 1px black",maxWidth:"100vw", display:"flex", flexDirection:"column", alignContent:"center"}} >
+            <h1 className="title red">achref</h1>
+            <br />
+            <div style={ {display:"flex", justifyContent:"space-around"} }>
+   <img src="imagelnPublic.png" alt="hello"/>
+
+   <img src={imageInSrc} alt="img"  width="400"/>
+   
+     </div>
+     </div>
+        <video width="320" height="240"   controls >
+        <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+        </div>
+        </div>
   );
+  
 }
 
+
+
 export default App;
+
+
